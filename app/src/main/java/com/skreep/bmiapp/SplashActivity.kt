bca5f060db6df3fad41728e5d38654c0d1a48534
skreep1 <skrypka.m.l@gmail.com>
2021-07-22 15:23:35 +0300

    Commit

--- app/src/main/java/com/skreep/bmiapp/SplashActivity.kt
+++ app/src/main/java/com/skreep/bmiapp/SplashActivity.kt
@@ -1,12 +1,26 @@
 package com.skreep.bmiapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setBackgroundSecond()
     }
 
+
+    private fun setBackgroundSecond() {
+
+        Handler(Looper.myLooper() ?: return).postDelayed({
+
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+
+        }, 2000)
+    }
 }
