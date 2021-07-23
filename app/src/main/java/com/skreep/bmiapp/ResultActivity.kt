6b66a57bf0d7d8713da1b8c9be0c1696a20c7cf6
skreep1 <skrypka.m.l@gmail.com>
2021-07-23 20:23:28 +0300

    Commit

---
+++ app/src/main/java/com/skreep/bmiapp/ResultActivity.kt
@@ -0,0 +1,14 @@
+package com.skreep.bmiapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+
+
+}
