15a93c1a842e15f717a2741c5cdbe68572a1bb55
skreep1 <skrypka.m.l@gmail.com>
2021-07-27 18:07:31 +0300

    Commit

--- app/src/main/java/com/skreep/bmiapp/HomeActivity.kt
+++ app/src/main/java/com/skreep/bmiapp/HomeActivity.kt
@@ -1,11 +1,46 @@
 package com.skreep.bmiapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.skreep.bmiapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.btn.setOnClickListener {
+
+            if (binding.etHeight.text.isEmpty() || binding.etWeight.text.isEmpty()) {
+
+                Toast.makeText(applicationContext, "Вы не указали данные", Toast.LENGTH_SHORT).show()
+
+            }
+
+            val height: Int = binding.etHeight.text.toString().toInt()
+            val weight: Int = binding.etWeight.text.toString().toInt()
+
+            val intent = Intent(this, ResultActivity::class.java)
+
+            intent.putExtra("height", height)
+            intent.putExtra("weight", weight)
+
+            startActivity(intent)
+
+
+        }
     }
+
 }
--- app/src/main/java/com/skreep/bmiapp/ResultActivity.kt
+++ app/src/main/java/com/skreep/bmiapp/ResultActivity.kt
@@ -3,12 +3,56 @@ package com.skreep.bmiapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.skreep.bmiapp.databinding.ActivityResultBinding
+import kotlin.math.pow
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+
+        binding = ActivityResultBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.icBack.setOnClickListener {
+            startActivity(Intent(this@ResultActivity, HomeActivity::class.java))
+
+        }
+
+        val height = intent.getIntExtra("height", 0)
+        val weight = intent.getIntExtra("weight", 0)
+
+        val bmiResult = weight / (height / 100.0).pow(2.0)
+
+        when (bmiResult) {
+            in 0.0..18.5 -> {
+                binding.tvBmiText.setText(R.string.underweight)
+                binding.tvBmiResult.setText(bmiResult.toString())
+            }
+            in 18.5..24.9 -> {
+                binding.tvBmiText.setText(R.string.normal)
+                binding.tvBmiResult.setText(bmiResult.toString())
+            }
+            in 25.0..29.9 -> {
+                binding.tvBmiText.setText(R.string.overweight)
+                binding.tvBmiResult.setText(bmiResult.toString())
+            }
+            in 30.0..50.0 -> {
+                binding.tvBmiText.setText(R.string.obese)
+                binding.tvBmiResult.setText(bmiResult.toString())
+            }
     }
 
 
-}
+
+        }
+
+    }
+
+
+
+
